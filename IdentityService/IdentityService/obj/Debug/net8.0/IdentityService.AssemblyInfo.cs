9b424a602420eda1fe21587dea3dc0283bbdddc1
CodebyJLP <lakshmi.jasti19@gmail.com>
2026-04-07 13:56:26 +0100

    Initial commit - Enterprise Order Management System

--- IdentityService/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ IdentityService/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0e206dc50be803beedfd1af80f40c70a0f2add")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
